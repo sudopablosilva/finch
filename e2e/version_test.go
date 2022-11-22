c4ce7a0bec1017b17cbbd23f1d9c64c00279b6e0
Ziwen Ning <ningziwe@amazon.com>
2022-11-22 15:24:40 -0800

    test

--- e2e/version_test.go
+++ e2e/version_test.go
@@ -5,7 +5,6 @@ package e2e
 
 import (
 	"os/exec"
-	"time"
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
@@ -20,7 +19,7 @@ var testVersion = func(o *option.Option) {
 			cmd := exec.Command("finch", "version") //nolint:gosec // G204 is not an issue because cmdName is fully controlled by the user.
 			session, err := gexec.Start(cmd, ginkgo.GinkgoWriter, ginkgo.GinkgoWriter)
 			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-			session.Wait(10 * time.Second)
+			//session.Wait(10 * time.Second)
 
 			gomega.Expect(session).Should(gexec.Exit(0))
 			//gomega.Expect(command.StdoutStr(o, "version")).To(gomega.Equal(fmt.Sprintf("Finch version: %s", "v0.1.0")))
