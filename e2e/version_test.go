0958361eb974e9a5800d62fb3a68868aa64c4405
Ziwen Ning <ningziwe@amazon.com>
2022-11-22 22:34:15 -0800

    Add checkpoint

--- e2e/version_test.go
+++ e2e/version_test.go
@@ -21,9 +21,13 @@ var testVersion = func(o *option.Option) {
 			//exec.Command("finch", "version").Start()
 			cmd := exec.Command("finch", "version") //nolint:gosec // G204 is not an issue because cmdName is fully controlled by the user.
 			session, err := gexec.Start(cmd, ginkgo.GinkgoWriter, ginkgo.GinkgoWriter)
+			ginkgo.GinkgoWriter.Println("checkpoint 1")
 			gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+			ginkgo.GinkgoWriter.Println("checkpoint 2")
 			session.Wait(10 * time.Second)
+			ginkgo.GinkgoWriter.Println("checkpoint 3")
 			gomega.Expect(session).Should(gexec.Exit(0))
+			ginkgo.GinkgoWriter.Println("checkpoint 4")
 			gomega.Expect(command.StdoutStr(o, "version")).To(gomega.Equal(fmt.Sprintf("Finch version: %s", "v0.1.0")))
 		})
 	})
