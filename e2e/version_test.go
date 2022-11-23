9bf30c57ef82b40ccd0476f2dd9cf1aaef4454bb
Ziwen Ning <ningziwe@amazon.com>
2022-11-23 00:18:19 -0800

    test

--- e2e/version_test.go
+++ e2e/version_test.go
@@ -16,6 +16,7 @@ var testVersion = func(o *option.Option) {
 			//exec.Command("finch", "version").Start()
 			cmd := exec.Command("finch", "version") //nolint:gosec // G204 is not an issue because cmdName is fully controlled by the user.
 			cmd.Run()
+			//b, err := afero.ReadFile(fs, fmt.Sprintf("%s/.finch/finch.yaml", os.Getenv("HOME")))
 			//session, err := gexec.Start(cmd, ginkgo.GinkgoWriter, ginkgo.GinkgoWriter)
 			//gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
 			//session.Wait(10 * time.Second)
