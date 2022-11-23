66fabb71da0e0a76876a2deb08bf76cd191c6e56
Ziwen Ning <ningziwe@amazon.com>
2022-11-23 00:22:49 -0800

    test read file

--- e2e/version_test.go
+++ e2e/version_test.go
@@ -4,19 +4,21 @@
 package e2e
 
 import (
-	"os/exec"
+	"fmt"
+	"os"
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/runfinch/common-tests/option"
+	"github.com/spf13/afero"
 )
 
 var testVersion = func(o *option.Option) {
 	ginkgo.Context("Version", func() {
 		ginkgo.Specify("Test version", func() {
 			//exec.Command("finch", "version").Start()
-			cmd := exec.Command("finch", "version") //nolint:gosec // G204 is not an issue because cmdName is fully controlled by the user.
-			cmd.Run()
-			//b, err := afero.ReadFile(fs, fmt.Sprintf("%s/.finch/finch.yaml", os.Getenv("HOME")))
+			//cmd := exec.Command("finch", "version") //nolint:gosec // G204 is not an issue because cmdName is fully controlled by the user.
+			//cmd.Run()
+			afero.ReadFile(afero.NewOsFs(), fmt.Sprintf("%s/.finch/finch.yaml", os.Getenv("HOME")))
 			//session, err := gexec.Start(cmd, ginkgo.GinkgoWriter, ginkgo.GinkgoWriter)
 			//gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
 			//session.Wait(10 * time.Second)
